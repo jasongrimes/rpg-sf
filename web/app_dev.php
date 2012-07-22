1f3d62849ca380d9a3aabd979c4a8db1b2812d5d
vagrant <vagrant@lamp-vm.(none)>
2012-07-22 18:39:35 +0000

    Tweak initial config and remove demo code.

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,7 +2,7 @@
 
 // if you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+umask(0000);
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
@@ -11,6 +11,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
+        '10.0.2.2',
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
